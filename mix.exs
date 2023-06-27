26f1a9fa1ca0f7336de28bcb1ef764cc5ba23177
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-06-26 22:29:08 -0300

    chore: include credo

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Api.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.22"},
       {:jason, "~> 1.4"},
-      {:plug_cowboy, "~> 2.6"}
+      {:plug_cowboy, "~> 2.6"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
