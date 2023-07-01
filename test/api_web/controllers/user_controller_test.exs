d584aa37549bdc3421fe39e5cd97338f81d340a8
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-06-30 23:33:54 -0300

    test: add auth for user controller tests

--- test/api_web/controllers/user_controller_test.exs
+++ test/api_web/controllers/user_controller_test.exs
@@ -3,21 +3,25 @@ defmodule ApiWeb.UserControllerTest do
 
   import Api.Factories.UserFactory
 
+  alias ApiWeb.Auth.AuthHandler
+
   @id_not_found Ecto.UUID.generate()
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    conn
+    |> put_req_header("accept", "application/json")
+    |> then(&{:ok, conn: &1})
   end
 
   describe "index/2 returns success" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "with a list of users when there are users", %{conn: conn, user: user} do
       %{id: id, email: email, name: name} = user
 
       conn = get(conn, ~p"/users")
 
-      assert %{"data" => [user]} = json_response(conn, 200)
+      assert %{"data" => [user]} = json_response(conn, :ok)
 
       assert user["id"] == id
       assert user["email"] == email
@@ -26,14 +30,14 @@ defmodule ApiWeb.UserControllerTest do
   end
 
   describe "create/2 returns success" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user parameters are valid", %{conn: conn} do
       user_params = params_for(:user)
 
       conn = post(conn, ~p"/users", user: user_params)
 
-      assert %{"data" => user_data} = json_response(conn, 201)
+      assert %{"data" => user_data} = json_response(conn, :created)
 
       assert user_data["email"] == user_params.email
       assert user_data["name"] == user_params.name
@@ -41,14 +45,14 @@ defmodule ApiWeb.UserControllerTest do
   end
 
   describe "create/2 returns error" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user parameters are invalid", %{conn: conn} do
       user_params = %{email: "???", name: nil, password: "?"}
 
       conn = post(conn, ~p"/users", user: user_params)
 
-      assert %{"errors" => errors} = json_response(conn, 422)
+      assert %{"errors" => errors} = json_response(conn, :unprocessable_entity)
 
       assert Enum.member?(errors["email"], "has invalid format")
       assert Enum.member?(errors["name"], "can't be blank")
@@ -57,12 +61,12 @@ defmodule ApiWeb.UserControllerTest do
   end
 
   describe "show/2 returns success" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user id is found", %{conn: conn, user: user} do
       conn = get(conn, ~p"/users/#{user}")
 
-      assert %{"data" => user_data} = json_response(conn, 200)
+      assert %{"data" => user_data} = json_response(conn, :ok)
 
       assert user_data["id"] == user.id
       assert user_data["email"] == user.email
@@ -71,26 +75,26 @@ defmodule ApiWeb.UserControllerTest do
   end
 
   describe "show/2 returns error" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user id is not found", %{conn: conn} do
       conn = get(conn, ~p"/users/#{@id_not_found}")
 
-      assert %{"errors" => errors} = json_response(conn, 422)
+      assert %{"errors" => errors} = json_response(conn, :unprocessable_entity)
 
       assert Enum.member?(errors["id"], "not found")
     end
   end
 
   describe "update/2 returns success" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user parameters are valid", %{conn: conn, user: user} do
       user_params = params_for(:user)
 
       conn = put(conn, ~p"/users/#{user}", user: user_params)
 
-      assert %{"data" => user_data} = json_response(conn, 200)
+      assert %{"data" => user_data} = json_response(conn, :ok)
 
       assert user_data["id"] == user.id
       assert user_data["email"] == user_params.email
@@ -99,14 +103,14 @@ defmodule ApiWeb.UserControllerTest do
   end
 
   describe "update/2 returns error" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user parameters are invalid", %{conn: conn, user: user} do
       user_params = %{email: "?@?", name: "?", password: nil}
 
       conn = put(conn, ~p"/users/#{user}", user: user_params)
 
-      assert %{"errors" => errors} = json_response(conn, 422)
+      assert %{"errors" => errors} = json_response(conn, :unprocessable_entity)
 
       assert Enum.member?(errors["email"], "has invalid format")
       assert Enum.member?(errors["name"], "should be at least 2 character(s)")
@@ -115,26 +119,22 @@ defmodule ApiWeb.UserControllerTest do
   end
 
   describe "delete/2 returns success" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user is found", %{conn: conn, user: user} do
       conn = delete(conn, ~p"/users/#{user}")
-      assert response(conn, 204)
 
-      conn = get(conn, ~p"/users/#{user}")
-      assert %{"errors" => errors} = json_response(conn, 422)
-
-      assert Enum.member?(errors["id"], "not found")
+      assert response(conn, :no_content)
     end
   end
 
   describe "delete/2 returns error" do
-    setup [:insert_user]
+    setup [:insert_user, :put_auth]
 
     test "when the user is not found", %{conn: conn} do
       conn = delete(conn, ~p"/users/#{@id_not_found}")
 
-      assert %{"errors" => errors} = json_response(conn, 422)
+      assert %{"errors" => errors} = json_response(conn, :unprocessable_entity)
 
       assert Enum.member?(errors["id"], "not found")
     end
@@ -143,7 +143,18 @@ defmodule ApiWeb.UserControllerTest do
   defp insert_user(_) do
     :user
     |> insert()
-    |> Map.put(:password, nil)
     |> then(&{:ok, user: &1})
+  end
+
+  defp put_auth(%{conn: conn, user: user}) do
+    {:ok, %{token: %{access: token}}} =
+      Map.new()
+      |> Map.put("email", user.email)
+      |> Map.put("password", user.password)
+      |> AuthHandler.authenticate_user()
+
+    conn
+    |> put_req_header("authorization", "Bearer #{token}")
+    |> then(&{:ok, conn: &1})
   end
 end
