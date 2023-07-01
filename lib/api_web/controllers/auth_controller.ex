adf744ee3e515418fc36486f3f50b95059c245dc
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-06-30 23:52:22 -0300

    feat: add auth controller

---
+++ lib/api_web/controllers/auth_controller.ex
@@ -0,0 +1,32 @@
+defmodule ApiWeb.AuthController do
+  @moduledoc """
+  Handles auth requests
+  """
+  use ApiWeb, :controller
+
+  alias Api.Accounts
+  alias ApiWeb.Auth.AuthHandler
+
+  action_fallback ApiWeb.FallbackController
+
+  @doc """
+  Handles requests to sign up.
+  """
+  def signup(conn, %{"user" => user_params}) do
+    with {:ok, _user} <- Accounts.create_user(user_params),
+         {:ok, auth} <- AuthHandler.authenticate_user(user_params) do
+      conn
+      |> put_status(:created)
+      |> render(:show, auth: auth)
+    end
+  end
+
+  @doc """
+  Handles requests to sign in.
+  """
+  def signin(conn, %{"credentials" => user_credentials}) do
+    with {:ok, auth} <- AuthHandler.authenticate_user(user_credentials) do
+      render(conn, :show, auth: auth)
+    end
+  end
+end
